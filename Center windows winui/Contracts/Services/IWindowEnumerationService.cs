ec18c8c8dd211c44ac615f889edefc3e4c2539b0
arthurits <simply_alfy@hotmail.com>
2025-07-12 14:29:42 +0200

    Create window enumeration contract

---
+++ Center windows winui/Contracts/Services/IWindowEnumerationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterWindow.Contracts.Services;
+public interface IWindowEnumerationService
+{
+    IEnumerable<WindowModel> GetDesktopWindows();
+}
