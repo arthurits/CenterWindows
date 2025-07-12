9f4d95a3fbcec932a4fd72fcb4f0b8ec42ed6fb8
arthurits <simply_alfy@hotmail.com>
2025-07-12 14:49:19 +0200

    Update window enumeration service

--- Center windows winui/Services/WindowEnumerationService.cs
+++ Center windows winui/Services/WindowEnumerationService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using CenterWindow.Contracts.Services;
+using CenterWindow.Interop;
 using CenterWindow.Models;
 
 namespace CenterWindow.Services;
@@ -7,29 +8,35 @@ public class WindowEnumerationService : IWindowEnumerationService
 {
     public IEnumerable<WindowModel> GetDesktopWindows()
     {
-        var result = new List<WindowModel>();
+        var windows = new List<WindowModel>();
 
-        EnumWindows((hwnd, _) =>
+        _ = NativeMethods.EnumWindows((hwnd, _) =>
         {
-            if (IsWindowVisible(hwnd))
+            if (!NativeMethods.IsWindowVisible(hwnd))
             {
-                // Obtener título
-                int length = GetWindowTextLength(hwnd);
-                var sb = new StringBuilder(length + 1);
-                GetWindowText(hwnd, sb, sb.Capacity);
-
-                // Obtener posición
-                var rect = new RECT();
-                GetWindowRect(hwnd, ref rect);
-                var bounds = new Rect(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
-
-                result.Add(new WindowModel(hwnd, sb.ToString(), bounds));
+                return true;
             }
+
+            // Título
+            var length = NativeMethods.GetWindowTextLength(hwnd);
+            var sb = new StringBuilder(length + 1);
+            _ = NativeMethods.GetWindowText(hwnd, sb, sb.Capacity);
+            var title = sb.ToString();
+
+            // Rectángulo
+            NativeMethods.RECT rc;
+            NativeMethods.GetWindowRect(hwnd, out rc);
+            var bounds = new WindowRect(
+                rc.Left,
+                rc.Top,
+                rc.Right - rc.Left,
+                rc.Bottom - rc.Top);
+
+            windows.Add(new WindowModel(hwnd, title, bounds));
             return true;
         }, IntPtr.Zero);
 
-        return result;
+        return windows;
     }
 
-    // Aquí van las declaraciones de PInvoke: EnumWindows, IsWindowVisible, GetWindowText, GetWindowRect, etc.
 }
