87f3082602876c1a90085275098dbbea97698735
arthurits <simply_alfy@hotmail.com>
2025-07-12 14:30:57 +0200

    Implement window enumeration service

--- Center windows winui/Contracts/Services/IWindowEnumerationService.cs
+++ Center windows winui/Contracts/Services/IWindowEnumerationService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CenterWindow.Contracts.Services;
+﻿namespace CenterWindow.Contracts.Services;
 public interface IWindowEnumerationService
 {
     IEnumerable<WindowModel> GetDesktopWindows();
---
+++ Center windows winui/Services/WindowEnumerationService.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using CenterWindow.Contracts.Services;
+
+namespace CenterWindow.Services;
+public class WindowEnumerationService : IWindowEnumerationService
+{
+    public IEnumerable<WindowModel> GetDesktopWindows()
+    {
+        var result = new List<WindowModel>();
+
+        EnumWindows((hwnd, _) =>
+        {
+            if (IsWindowVisible(hwnd))
+            {
+                // Obtener título
+                int length = GetWindowTextLength(hwnd);
+                var sb = new StringBuilder(length + 1);
+                GetWindowText(hwnd, sb, sb.Capacity);
+
+                // Obtener posición
+                var rect = new RECT();
+                GetWindowRect(hwnd, ref rect);
+                var bounds = new Rect(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+
+                result.Add(new WindowModel(hwnd, sb.ToString(), bounds));
+            }
+            return true;
+        }, IntPtr.Zero);
+
+        return result;
+    }
+
+    // Aquí van las declaraciones de PInvoke: EnumWindows, IsWindowVisible, GetWindowText, GetWindowRect, etc.
+}
