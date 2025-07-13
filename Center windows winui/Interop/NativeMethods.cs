fe38c307b49a6b5a34b5fa04674e977a4d18ae82
arthurits <simply_alfy@hotmail.com>
2025-07-13 11:27:11 +0200

    Reference native methods in window center service

--- Center windows winui/Interop/NativeMethods.cs
+++ Center windows winui/Interop/NativeMethods.cs
@@ -23,16 +23,16 @@ internal static class NativeMethods
     public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
 
     [DllImport("user32.dll")]
-    static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 
     [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLongPtrW")]
-    static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+    public static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
 
     [DllImport("user32.dll", SetLastError = true)]
-    static extern bool SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
+    public static extern bool SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
 
     [DllImport("user32.dll")]
-    static extern int GetSystemMetrics(int nIndex);
+    public static extern int GetSystemMetrics(int nIndex);
 
 
     [StructLayout(LayoutKind.Sequential)]
--- Center windows winui/Services/WindowCenterService.cs
+++ Center windows winui/Services/WindowCenterService.cs
@@ -1,48 +1,35 @@
-﻿using System.Runtime.InteropServices;
-using CenterWindow.Contracts.Services;
+﻿using CenterWindow.Contracts.Services;
+using CenterWindow.Interop;
 
 namespace CenterWindow.Services;
 public class WindowCenterService : IWindowCenterService
 {
-    // Ya tienes definido RECT en el Core; lo reutilizamos.
-    [DllImport("user32.dll")]
-    static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-    [DllImport("user32.dll")]
-    static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-
-    [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLongPtrW")]
-    static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern bool SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
-
-    [DllImport("user32.dll")]
-    static extern int GetSystemMetrics(int nIndex);
-
-    const int GWL_EXSTYLE = -20;
-    const int WS_EX_LAYERED = 0x00080000;
-    const uint LWA_ALPHA = 0x02;
-    const int SM_CXSCREEN = 0;
-    const int SM_CYSCREEN = 1;
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_LAYERED = 0x00080000;
+    private const uint LWA_ALPHA = 0x02;
+    private const int SM_CXSCREEN = 0;
+    private const int SM_CYSCREEN = 1;
 
     public void CenterWindow(IntPtr hWnd, byte alpha)
     {
-        if (!GetWindowRect(hWnd, out var rect)) return;
+        if (!NativeMethods.GetWindowRect(hWnd, out var rect))
+        {
+            return;
+        }
 
-        int width = rect.Right  - rect.Left;
-        int height = rect.Bottom - rect.Top;
+        var width = rect.Right  - rect.Left;
+        var height = rect.Bottom - rect.Top;
 
-        int screenW = GetSystemMetrics(SM_CXSCREEN);
-        int screenH = GetSystemMetrics(SM_CYSCREEN);
+        var screenW = NativeMethods.GetSystemMetrics(SM_CXSCREEN);
+        var screenH = NativeMethods.GetSystemMetrics(SM_CYSCREEN);
 
-        int x = (screenW - width)  / 2;
-        int y = (screenH - height) / 2;
+        var x = (screenW - width)  / 2;
+        var y = (screenH - height) / 2;
 
-        MoveWindow(hWnd, x, y, width, height, true);
+        NativeMethods.MoveWindow(hWnd, x, y, width, height, true);
 
-        // Habilitar estilo Layered y aplicar alpha
-        SetWindowLongPtr(hWnd, GWL_EXSTYLE, new IntPtr(WS_EX_LAYERED));
-        SetLayeredWindowAttributes(hWnd, 0, alpha, LWA_ALPHA);
+        // Enable layered style and apply alpha blending
+        NativeMethods.SetWindowLongPtr(hWnd, GWL_EXSTYLE, new IntPtr(WS_EX_LAYERED));
+        NativeMethods.SetLayeredWindowAttributes(hWnd, 0, alpha, LWA_ALPHA);
     }
 }
