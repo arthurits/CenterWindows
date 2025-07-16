0cb3db60f9e28658ab71e3fe6945fd8f9973dc06
arthurits <simply_alfy@hotmail.com>
2025-07-16 12:15:28 +0200

    Add localization service

---
+++ Center windows winui/Contracts/Services/ILocalizationService.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using HeatStress.Models;
+
+namespace HeatStress.Contracts.Services;
+
+public interface ILocalizationService
+{
+    public string CurrentLanguage { get; }
+    event EventHandler LanguageChanged;
+    string GetString(string key);
+    string GetString(string key, string resourceMap);
+    void SetAppLanguage(string languageCode, string invariantCulture = "en", bool notifyLanguageChanged = true);
+    IEnumerable<CultureOption> GetAvailableLanguages(
+        string resourceFileName = "Resources",
+        bool invariantCulture = true,
+        bool loopAllKeys = false,
+        CultureInfo? targetCulture = null);
+}
