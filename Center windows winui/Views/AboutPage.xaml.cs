1e248b5be5379a56cf1c0a7fae24a1cc338357b0
arthurits <simply_alfy@hotmail.com>
2025-07-16 12:09:47 +0200

    Add about page view

---
+++ Center windows winui/Views/AboutPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using HeatStress.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace HeatStress.Views;
+
+public sealed partial class AboutPage : Page
+{
+    public AboutViewModel AboutViewModel { get; }
+
+    public AboutPage()
+    {
+        InitializeComponent();
+        AboutViewModel = App.GetService<AboutViewModel>();
+
+        // Set the DataContext to the ViewModel
+        DataContext = AboutViewModel;
+    }
+}
