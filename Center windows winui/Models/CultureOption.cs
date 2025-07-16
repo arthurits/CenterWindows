5cc1974b12078a6f8d8e024ba1a116ee37c9ffb4
arthurits <simply_alfy@hotmail.com>
2025-07-16 12:17:10 +0200

    Add culture option model

---
+++ Center windows winui/Models/CultureOption.cs
@@ -0,0 +1,12 @@
+﻿namespace HeatStress.Models;
+
+public class CultureOption
+{
+    public string DisplayName { get; set; } = string.Empty;
+    public string LanguageTag { get; set; } = string.Empty;
+
+    public override string ToString()
+    {
+        return $"{DisplayName} ({LanguageTag})";
+    }
+}
