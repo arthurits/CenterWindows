164c81961e31e124a1efff26f9e5230578a1fc94
arthurits <simply_alfy@hotmail.com>
2025-07-16 12:25:01 +0200

    Modify resource extensions

--- Center windows winui/Helpers/ResourceExtensions.cs
+++ Center windows winui/Helpers/ResourceExtensions.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Windows.ApplicationModel.Resources;
+﻿using CenterWindow.Contracts.Services;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace CenterWindow.Helpers;
 
 public static class ResourceExtensions
 {
     private static readonly ResourceLoader _resourceLoader = new();
+    public static readonly ILocalizationService _localizationService = App.GetService<ILocalizationService>();
 
     public static string GetLocalized(this string resourceKey) => _resourceLoader.GetString(resourceKey);
+    public static string GetLocalized(this string resourceKey, string resourceMap) => _localizationService.GetString(resourceKey, resourceMap);
 }
