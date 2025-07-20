4092ff5dffe28fd955ebeaeaaf843fc0cc0569e9
arthurits <simply_alfy@hotmail.com>
2025-07-20 11:41:54 +0200

    Create TrayMenuOpeningEventArgs

---
+++ Center windows winui/Services/TrayMenuOpeningEventArgs.cs
@@ -0,0 +1,8 @@
+﻿using CenterWindow.Models;
+
+namespace CenterWindow.Services;
+
+public class TrayMenuOpeningEventArgs : EventArgs
+{
+    public List<TrayMenuItemDefinition> Items { get; } = [];
+}
