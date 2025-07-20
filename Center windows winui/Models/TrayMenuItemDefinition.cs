f8767abbd50c16cc78c590ee46c28af47aa43b6b
arthurits <simply_alfy@hotmail.com>
2025-07-20 11:40:21 +0200

    Add tray menu item model

---
+++ Center windows winui/Models/TrayMenuItemDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace CenterWindow.Models;
+
+public class TrayMenuItemDefinition
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public List<TrayMenuItemDefinition> Children { get; } = [];
+}
