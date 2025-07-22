97a5020fa8eda8bb8c0d28ba23f439b39d4d2ccf
arthurits <simply_alfy@hotmail.com>
2025-07-22 20:22:26 +0200

    Modify icon loader contract to include the size

--- Center windows winui/Contracts/Services/IIconLoader.cs.cs
+++ Center windows winui/Contracts/Services/IIconLoader.cs.cs
@@ -6,8 +6,9 @@ public interface IIconLoader
     /// ready to be used.
     /// </summary>
     /// <param name="path">The file path of the image to load (png, jpg, etc.).</param>
+    /// <param name="size">The size of the icon to load, typically 16, 32, or 48 pixels.</param>
     /// <returns>A task that represents the asynchronous operation. The task result is a handle to the loaded icon as an <see
     /// cref="IntPtr"/>.</returns>
-    Task<IntPtr> LoadIconAsync(string path);
+    Task<IntPtr> LoadIconAsync(string path, uint size);
 
 }
