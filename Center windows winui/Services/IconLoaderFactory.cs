d079c50b17f41a9449888c0ea72cb7b078b03b28
arthurits <simply_alfy@hotmail.com>
2025-07-22 20:53:36 +0200

    Use primary constructor in icon loader factory

--- Center windows winui/Services/IconLoaderFactory.cs
+++ Center windows winui/Services/IconLoaderFactory.cs
@@ -1,22 +1,19 @@
 ﻿using CenterWindow.Contracts.Services;
 
 namespace CenterWindow.Services;
-public class IconLoaderFactory : IIconLoaderFactory
+public class IconLoaderFactory(GdiPlusIconLoader gdi, Win2DIconLoader win2d) : IIconLoaderFactory
 {
-    private readonly GdiPlusIconLoader _gdi;
-    private readonly Win2DIconLoader _win2d;
+    private readonly GdiPlusIconLoader _gdi = gdi;
+    private readonly Win2DIconLoader _win2d = win2d;
 
-    public IconLoaderFactory(GdiPlusIconLoader gdi, Win2DIconLoader win2d)
+    public IIconLoader GetLoader(IconLoaderType type)
     {
-        _gdi   = gdi;
-        _win2d = win2d;
-    }
-
-    public IIconLoader GetLoader(IconLoaderType type) =>
-        type switch
+        IIconLoader value = type switch
         {
             IconLoaderType.GdiPlus => _gdi,
             IconLoaderType.Win2D => _win2d,
             _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
         };
+        return value;
+    }
 }
