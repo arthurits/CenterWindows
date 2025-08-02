5b4451c0ab9102a2666e595951332027af25ad15
arthurits <simply_alfy@hotmail.com>
2025-08-02 18:10:31 +0200

    Define binding variables in list windows view model

--- Center windows winui/ViewModels/ListWindowsViewModel_String.cs
+++ Center windows winui/ViewModels/ListWindowsViewModel_String.cs
@@ -40,8 +40,13 @@ public partial class ListWindowsViewModel : ObservableRecipient
     [ObservableProperty]
     public partial string StrRefreshToolTip { get; set; } = string.Empty;
 
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StrTransparencyText))]
+    public partial string StrTransparencyHeader { get; set; } = string.Empty;
+
     private void OnLanguageChanged(object? sender, EventArgs e)
     {
+        // Flyout menu and CommandBar buttons
         StrCenter = "StrCenter".GetLocalized("ListWindows");
         StrCenterToolTip = "StrCenterToolTip".GetLocalized("ListWindows");
         StrTransparency = "StrTransparency".GetLocalized("ListWindows");
@@ -54,5 +59,8 @@ public partial class ListWindowsViewModel : ObservableRecipient
         StrDeselectToolTip = "StrDeselectToolTip".GetLocalized("ListWindows");
         StrRefresh = "StrRefresh".GetLocalized("ListWindows");
         StrRefreshToolTip = "StrRefreshToolTip".GetLocalized("ListWindows");
+
+        // Header for the transparency slider
+        StrTransparencyHeader = "StrTransparencyHeader".GetLocalized("ListWindows");
     }
 }
