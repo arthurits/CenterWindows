637a94c18eb279844e87b1b5bc3a8efc4a334801
arthurits <simply_alfy@hotmail.com>
2025-08-02 19:12:38 +0200

    Define localization variable

--- Center windows winui/ViewModels/SelectWindowViewModel_Strings.cs
+++ Center windows winui/ViewModels/SelectWindowViewModel_Strings.cs
@@ -5,11 +5,17 @@ namespace CenterWindow.ViewModels;
 public partial class SelectWindowViewModel : ObservableRecipient, IDisposable
 {
     [ObservableProperty]
+    public partial string StrDragCrossHair { get; set; } = string.Empty;
+
+    [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(StrTransparencyText))]
     public partial string StrTransparencyHeader { get; set; } = string.Empty;
 
     private void OnLanguageChanged(object? sender, EventArgs e)
     {
+        // Header for the crosshair drag mode
+        StrDragCrossHair = "StrDragCrossHair".GetLocalized("SelectWindow");
+
         // Header for the transparency slider
         StrTransparencyHeader = "StrTransparencyHeader".GetLocalized("SelectWindow");
     }
