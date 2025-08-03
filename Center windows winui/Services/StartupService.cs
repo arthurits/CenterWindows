17055557c3137b7e3404800d98b316671a1e8680
arthurits <simply_alfy@hotmail.com>
2025-08-03 12:48:34 +0200

    Implement startup service

---
+++ Center windows winui/Services/StartupService.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using CenterWindow.Contracts.Services;
+using Microsoft.Win32;
+
+namespace CenterWindow.Services;
+public class StartupService : IStartupService
+{
+    private const string RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+    private const string AppName = "CenterWindows";
+
+    private static string ExecutablePath => Process.GetCurrentProcess().MainModule.FileName;
+
+    public void SetStartupEnabled(bool enabled)
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath, writable: true);
+
+        if (enabled)
+        {
+            key?.SetValue(AppName, ExecutablePath);
+        }
+        else
+        {
+            key?.DeleteValue(AppName, throwOnMissingValue: false);
+        }
+    }
+
+    public bool IsStartupEnabled()
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath);
+        var value = key?.GetValue(AppName) as string;
+        return value == ExecutablePath;
+    }
+}
