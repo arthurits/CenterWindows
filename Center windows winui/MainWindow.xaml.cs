81111726b7230ffd8431995a9e033a5e1eca0de5
arthurits <simply_alfy@hotmail.com>
2025-08-03 09:05:25 +0200

    Handle tray icon from ListWindow view model

--- Center windows winui/MainWindow.xaml.cs
+++ Center windows winui/MainWindow.xaml.cs
@@ -12,10 +12,6 @@ public sealed partial class MainWindow : WindowEx
 
     private readonly UISettings settings;
 
-    // Services
-    private readonly ITrayIconService _trayMenuService;
-    private readonly IMainWindowService _mainWindowService;
-
     public MainWindow()
     {
         InitializeComponent();
@@ -28,11 +24,6 @@ public sealed partial class MainWindow : WindowEx
         dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
         settings = new UISettings();
         settings.ColorValuesChanged += Settings_ColorValuesChanged; // cannot use FrameworkElement.ActualThemeChanged event
-
-        // Get services
-        _trayMenuService   = App.GetService<ITrayIconService>();
-        _mainWindowService = App.GetService<IMainWindowService>();
-        _trayMenuService.TrayMenuItemClicked += OnTrayMenuItemClicked;
     }
 
     // this handles updating the caption button colors correctly when indows system theme is changed
@@ -41,18 +32,5 @@ public sealed partial class MainWindow : WindowEx
     {
         // This calls comes off-thread, hence we will need to dispatch it to current app's thread
         dispatcherQueue.TryEnqueue(TitleBarHelper.ApplySystemThemeToCaptionButtons);
-    }
-
-    private void OnTrayMenuItemClicked(object? sender, TrayMenuItemEventArgs e)
-    {
-        switch (e.ItemId)
-        {
-            case (int)TrayMenuItemId.Open:
-                _mainWindowService.Show();
-                break;
-            case (int)TrayMenuItemId.Exit:
-                App.Current.Exit();
-                break;
-        }
     }
 }
