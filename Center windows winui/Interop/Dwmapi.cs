340cc25521d6d45bfde79d16ce2bb2e99794e843
arthurits <simply_alfy@hotmail.com>
2025-08-04 13:02:57 +0200

    Add DWM api example-code for future improvements

--- Center windows winui/Interop/Dwmapi.cs
+++ Center windows winui/Interop/Dwmapi.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using WinRT.Interop;
 
 namespace CenterWindow.Interop;
 internal static partial class Win32
@@ -20,3 +21,38 @@ internal static partial class Win32
         ref uint pvAttribute,
         int cbAttribute);
 }
+
+//private void InitializeWindowEffects()
+//    {
+//        IntPtr hwnd = WindowNative.GetWindowHandle(this);
+
+//        // 1. Esquinas redondeadas (Windows 11)
+//        uint cornerPref = (uint)DWM_WINDOW_CORNER_PREFERENCE.ROUND;
+//        DwmApi.DwmSetWindowAttribute(
+//            hwnd,
+//            DWMWINDOWATTRIBUTE.WINDOW_CORNER_PREFERENCE,
+//            ref cornerPref,
+//            sizeof(uint));
+
+//        // 2. Modo oscuro inmersivo (Win10 1809+)
+//        uint darkMode = 1;
+//        DwmApi.DwmSetWindowAttribute(
+//            hwnd,
+//            DWMWINDOWATTRIBUTE.USE_IMMERSIVE_DARK_MODE,
+//            ref darkMode,
+//            sizeof(uint));
+
+//        // 3. Extender frame para Acrylic / Aero
+//        var margins = new MARGINS { cxLeftWidth = -1, cxRightWidth = -1, cyTopHeight = -1, cyBottomHeight = -1 };
+//        DwmApi.DwmExtendFrameIntoClientArea(hwnd, ref margins);
+
+//        // 4. Blur behind clásico (opcional)
+//        var blur = new DWM_BLURBEHIND
+//        {
+//            dwFlags = DwmBlurBehindFlags.ENABLE,
+//            fEnable = true,
+//            hRgnBlur = IntPtr.Zero,
+//            fTransitionOnMaximized = false
+//        };
+//        DwmApi.DwmEnableBlurBehindWindow(hwnd, ref blur);
+//    }
