193988c9b790ba6a3f42a4ee087bbbdb4a2fbb93
arthurits <simply_alfy@hotmail.com>
2025-08-07 16:27:48 +0200

    Add localized properties to select window view model

--- Center windows winui/ViewModels/SelectWindowViewModel_Strings.cs
+++ Center windows winui/ViewModels/SelectWindowViewModel_Strings.cs
@@ -11,6 +11,19 @@ public partial class SelectWindowViewModel : ObservableRecipient, IDisposable
     [NotifyPropertyChangedFor(nameof(StrTransparencyText))]
     public partial string StrTransparencyHeader { get; set; } = string.Empty;
 
+    [ObservableProperty]
+    public partial string StrWindowText { get; set; } = string.Empty;
+    [ObservableProperty]
+    public partial string StrWindowHandle { get; set; } = string.Empty;
+    [ObservableProperty]
+    public partial string StrWindowModuleName { get; set; } = string.Empty;
+    [ObservableProperty]
+    public partial string StrWindowClassName { get; set; } = string.Empty;
+    [ObservableProperty]
+    public partial string StrWindowLocation { get; set; } = string.Empty;
+    [ObservableProperty]
+    public partial string StrWindowDimensions { get; set; } = string.Empty;
+
     private void OnLanguageChanged(object? sender, EventArgs e)
     {
         // Header for the crosshair drag mode
@@ -18,5 +31,13 @@ public partial class SelectWindowViewModel : ObservableRecipient, IDisposable
 
         // Header for the transparency slider
         StrTransparencyHeader = "StrTransparencyHeader".GetLocalized("SelectWindow");
+
+        // Window output properties headers
+        StrWindowText = "StrWindowText".GetLocalized("SelectWindow");
+        StrWindowHandle = "StrWindowHandle".GetLocalized("SelectWindow");
+        StrWindowModuleName = "StrWindowModuleName".GetLocalized("SelectWindow");
+        StrWindowClassName = "StrWindowClassName".GetLocalized("SelectWindow");
+        StrWindowLocation = "StrWindowLocation".GetLocalized("SelectWindow");
+        StrWindowDimensions = "StrWindowDimensions".GetLocalized("SelectWindow");
     }
 }
